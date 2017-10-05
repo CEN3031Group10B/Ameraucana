cca32bcc62c2deb4eaf4925df2ebd76416b0c623
Travis DeMint <tdemint@ufl.edu>
2017-10-05 13:38:34 -0400

    still working on connecting to database

---
+++ modules/core/client/controllers/login.client.controller.js
@@ -0,0 +1,16 @@
+angular.module().controller('LoginController', ['$scope',
+  function($scope) {
+    $scope.email = '';
+    $scope.password = '';
+
+    /* Functions to implement */
+    $scope.loginUser = function() {
+      // Get document with email specified
+      // Return error if email doesn't exist
+
+      // Compare password provided with one saved
+      // Return error if passwords don't match
+      
+    }
+  }
+])
