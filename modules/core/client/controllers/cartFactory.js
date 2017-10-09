aa416ffb8618fe9b930eb9b96f9b0fcd3a8f3ee0
Kevin Almanzar <kevinalmanzar8@gmail.com>
2017-10-09 13:17:33 -0400

    Check Out Page mostly complete. Missing connection to DB.

---
+++ modules/core/client/controllers/cartFactory.js
@@ -0,0 +1,22 @@
+/*'use strict';
+
+angular.module('core').factory('Cart', function() {
+    var cart = {
+    entries : [
+        {
+            "code": "MEAT", 
+            "name": "Meatballs", 
+            "price": "100",
+            "quantity": "1",
+            "image": "/modules/core/client/img/banner.jpg"
+        },
+        {
+            "code": "PIZZA", 
+            "name": "Cheese Pizza", 
+            "price": "200",
+            "quantity": "1",
+            "image": "/modules/core/client/img/banner.jpg"
+        }
+    ]};
+        return cart.entries;
+}); */
