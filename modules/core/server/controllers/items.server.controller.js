0e68d680ea07a83f0c8db7d7ecf15eae6a275651
Franky Liang <fliangz96@gmail.com>
2017-10-18 12:41:10 -0400

    Add items analytics

--- modules/core/server/controllers/items.server.controller.js
+++ modules/core/server/controllers/items.server.controller.js
@@ -1,5 +1,38 @@
 var mongoose = require('mongoose');
-var item = require('../models/items.server.model.js');
-
+var Item = require('../models/items.server.model.js');
+var User = require('../../../users/server/models/user.server.model.js');
+// http://mongoosejs.com/docs/populate.html
 /* Here is where you will implement any functions you need
    to access/change anything from the item table */
+exports.getItemsAnalytics = function(req, res) {
+  Item.find({}, '', function (err, items) {
+    /* Do something with all the items
+    */
+    if(err) {
+      res.status(400).send(err);
+    } else {
+      var itemsAnalytics = []
+
+      items.forEach(function (currentItem) {
+        var count = 0
+
+        User.find({}, 'orders', function(err, orders) {
+
+            orders.forEach(function (err, order) {
+              if (order.order == currentItem) {
+                count += 1
+              }
+            });
+
+        });
+        itemsAnalytics.push({
+          item: currentItem,
+          count: count
+        });
+      });
+
+      res.send(itemsAnalytics);
+    }
+  });
+
+};
