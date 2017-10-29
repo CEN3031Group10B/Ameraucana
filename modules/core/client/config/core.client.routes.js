de6e10e29c6943bffeed81b2e016b09d9cf990ce
Travis DeMint <tdemint@ufl.edu>
2017-10-29 14:31:21 -0400

    title and logo are correct for the tab

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -48,7 +48,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 
       }
     })
-    
+
     .state('checkout', {
       url: '/checkout',
       templateUrl: 'modules/core/client/views/checkout.client.view.html',
@@ -59,7 +59,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
     .state('admin-panel', {
       url: '/admin-panel',
       templateUrl: 'modules/core/client/views/adminPanel.client.view.html'
-      
+
     });
   }
 ]);
