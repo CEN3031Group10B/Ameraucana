99584e80b32b359441c7395195be949041cba2e1
cecrigope <cecrigope@hotmail.com>
2017-11-28 11:10:29 -0500

    Began working on module testing

---
+++ modules/tests/module.test.js
@@ -0,0 +1,26 @@
+describe("Midway: Testing Modules", function() {
+  describe("App Module:", function() {
+
+    var module;
+    before(function() {
+      module = angular.module("App");
+    });
+
+    it("should be registered", function() {
+      expect(module).not.to.equal(null);
+    });
+
+    describe("Dependencis:", function() {
+      var deps;
+      var hasModule = function(m) {
+        return deps.indexOf(m) >= 0;
+      };
+
+      before(function() {
+        deps = module.value('appName').requires;
+      });
+
+      it()
+    })
+  })
+})
